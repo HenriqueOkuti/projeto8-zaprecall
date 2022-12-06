5800ad9f51057a382c28968611d287b0c540155b
Henrique <henrique.krastins@gmail.com>
2022-12-06 12:16:56 -0300

    feat: readme text added

--- src/Components/QuestionAnswered.js
+++ src/Components/QuestionAnswered.js
@@ -1,42 +1,65 @@
-export default function QuestionAnswered(question, image, setState, answered, setAnswered, index, setSize) {
-    return (
-        <div className="question_answered">
-            <div className="question_answer">
-                {question.answer}
-            </div>
-            <div className="answer_options">
-                <div onClick={() => { defineStates('R', setState, answered, setAnswered, index, setSize);}} className="red_option">
-                    Não lembrei!
-                </div>
-                <div onClick={() => { defineStates('O', setState, answered, setAnswered, index, setSize);}} className="orange_option">
-                    Quase não lembrei!
-                </div>
-                <div onClick={() => { defineStates('G', setState, answered, setAnswered, index, setSize);}} className="green_option">
-                    Zap!
-                </div>
-            </div>
-        </div >
-    );
-};
+import React from 'react';
+
+export default function QuestionAnswered(
+  question,
+  image,
+  setState,
+  answered,
+  setAnswered,
+  index,
+  setSize
+) {
+  return (
+    <div className="question_answered">
+      <div className="question_answer">{question.answer}</div>
+      <div className="answer_options">
+        <div
+          onClick={() => {
+            defineStates('R', setState, answered, setAnswered, index, setSize);
+          }}
+          className="red_option"
+        >
+          Não lembrei!
+        </div>
+        <div
+          onClick={() => {
+            defineStates('O', setState, answered, setAnswered, index, setSize);
+          }}
+          className="orange_option"
+        >
+          Quase não lembrei!
+        </div>
+        <div
+          onClick={() => {
+            defineStates('G', setState, answered, setAnswered, index, setSize);
+          }}
+          className="green_option"
+        >
+          Zap!
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function defineStates(option, setState, answered, setAnswered, index, setSize) {
-    updateAnswered(option, answered, setAnswered);
-    updateSize(option, index, setSize);
-    if (option === 'R') {
-        return setState([0, 'R']);
-    };
-    if (option === 'O') {
-        return setState([0, 'O']);
-    };
-    if (option === 'G') {
-        return setState([0, 'G']);
-    };
-};
+  updateAnswered(option, answered, setAnswered);
+  updateSize(option, index, setSize);
+  if (option === 'R') {
+    return setState([0, 'R']);
+  }
+  if (option === 'O') {
+    return setState([0, 'O']);
+  }
+  if (option === 'G') {
+    return setState([0, 'G']);
+  }
+}
 
 function updateAnswered(option, answered, setAnswered) {
-    return setAnswered(option);
-};
+  return setAnswered(option);
+}
 
 function updateSize(option, index, setSize) {
-    return setSize([index, option]);
-};
+  return setSize([index, option]);
+}
--- src/Components/Recall.js
+++ src/Components/Recall.js
@@ -1,6 +1,6 @@
 import Logo from '../resources/zapLogo.svg';
 import Select from '../resources/select.svg';
-import Flip from '../resources/flip.svg'
+import Flip from '../resources/flip.svg';
 import Question from './Question';
 import QuestionSelected from './QuestionSelected';
 import QuestionAnswered from './QuestionAnswered';
@@ -8,158 +8,169 @@ import Header from './Header';
 import Footer from './Footer';
 import react from 'react';
 
-
-
 const deck_fib = [
-    {
-        question: "1+1?",
-        answer: '2'
-    },
-    {
-        question: "1+2",
-        answer: '3'
-    },
-    {
-        question: "2+3?",
-        answer: '5'
-    },
-    {
-        question: "3+5?",
-        answer: '8'
-    }
+  {
+    question: '1+1?',
+    answer: '2',
+  },
+  {
+    question: '1+2',
+    answer: '3',
+  },
+  {
+    question: '2+3?',
+    answer: '5',
+  },
+  {
+    question: '3+5?',
+    answer: '8',
+  },
 ];
 
 const deck_fat = [
-    {
-        question: "1!",
-        answer: '1'
-    },
-    {
-        question: "2!",
-        answer: '2'
-    },
-    {
-        question: "3!",
-        answer: '6'
-    },
-    {
-        question: "4!",
-        answer: '24'
-    },
-    {
-        question: "5!",
-        answer: '120'
-    }
+  {
+    question: '1!',
+    answer: '1',
+  },
+  {
+    question: '2!',
+    answer: '2',
+  },
+  {
+    question: '3!',
+    answer: '6',
+  },
+  {
+    question: '4!',
+    answer: '24',
+  },
+  {
+    question: '5!',
+    answer: '120',
+  },
 ];
 
 const deckJSX = [
-    {
-        question: "O que é JSX?",
-        answer: 'Uma extensão de linguagem do JavaScript'
-    },
-    {
-        question: "O React é __",
-        answer: 'uma biblioteca JavaScript para construção de interfaces'
-    },
-    {
-        question: "Componentes devem iniciar com __",
-        answer: 'letra maiúscula'
-    },
-    {
-        question: "Podemos colocar __ dentro do JSX",
-        answer: 'expressões'
-    },
-    {
-        question: "O ReactDOM nos ajuda __",
-        answer: 'interagindo com a DOM para colocar componentes React na mesma'
-    },
-    {
-        question: "Usamos o npm para __",
-        answer: 'gerenciar os pacotes necessários e suas dependências'
-    },
-    {
-        question: "Usamos props para __",
-        answer: 'passar diferentes informações para componentes '
-    },
-    {
-        question: "Usamos estado (state) para __",
-        answer: 'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente'
-    },
+  {
+    question: 'O que é JSX?',
+    answer: 'Uma extensão de linguagem do JavaScript',
+  },
+  {
+    question: 'O React é __',
+    answer: 'uma biblioteca JavaScript para construção de interfaces',
+  },
+  {
+    question: 'Componentes devem iniciar com __',
+    answer: 'letra maiúscula',
+  },
+  {
+    question: 'Podemos colocar __ dentro do JSX',
+    answer: 'expressões',
+  },
+  {
+    question: 'O ReactDOM nos ajuda __',
+    answer: 'interagindo com a DOM para colocar componentes React na mesma',
+  },
+  {
+    question: 'Usamos o npm para __',
+    answer: 'gerenciar os pacotes necessários e suas dependências',
+  },
+  {
+    question: 'Usamos props para __',
+    answer: 'passar diferentes informações para componentes ',
+  },
+  {
+    question: 'Usamos estado (state) para __',
+    answer:
+      'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente',
+  },
 ];
 
 const deck_options = [
-    {
-        deck: deck_fib,
-        deck_name: 'deck_fib'
-    },
-    {
-        deck: deck_fat,
-        deck_name: 'deck_fat'
-    },
-    {
-        deck: deckJSX,
-        deck_name: 'deckJSX'
-    }
+  {
+    deck: deck_fib,
+    deck_name: 'deck_fib',
+  },
+  {
+    deck: deck_fat,
+    deck_name: 'deck_fat',
+  },
+  {
+    deck: deckJSX,
+    deck_name: 'deckJSX',
+  },
 ];
 let deck_recall = [];
 
 function FindDeck(name) {
-    const deck_chosen = deck_options.filter((e, i) => e.deck_name === name ? e : 0);
-    return deck_chosen[0].deck;
+  const deck_chosen = deck_options.filter((e, i) =>
+    e.deck_name === name ? e : 0
+  );
+  return deck_chosen[0].deck;
 }
 
 export default function Recall({ deckRecall, setStatus }) {
-    const [shuffled, setShuffled] = react.useState(false)
-    const [result, setResult] = react.useState([]);
+  const [shuffled, setShuffled] = react.useState(false);
+  const [result, setResult] = react.useState([]);
 
-    if (result === deck_recall.length && result !== []){
-        setResult([]);
-    }
+  if (result === deck_recall.length && result !== []) {
+    setResult([]);
+  }
 
-    if (shuffled === false) {
-        deck_recall = ChooseDeck(FindDeck(deckRecall));
-        setShuffled(true);
+  if (shuffled === false) {
+    deck_recall = ChooseDeck(FindDeck(deckRecall));
+    setShuffled(true);
+  }
+  if (true) {
+    if (result.length > deck_recall.length) {
+      console.log(result);
+      //array = [];
     }
-    if (true) {
-        if (result.length > deck_recall.length) {
-            console.log(result);
-            //array = [];
-        }
-    };
-    return (
-        <div className="recall_container">
-            <Header image={Logo} />
-            <div className="questions">
-                {deck_recall.map((e, i) => RenderCards(e, i, result))}
-            </div>
-            <Footer result={result} deck_length={deck_recall.length} setStatus={setStatus} />
-        </div>
-    );
-};
+  }
+  return (
+    <div className="recall_container">
+      <Header image={Logo} />
+      <div className="questions">
+        {deck_recall.map((e, i) => RenderCards(e, i, result))}
+      </div>
+      <Footer
+        result={result}
+        deck_length={deck_recall.length}
+        setStatus={setStatus}
+      />
+    </div>
+  );
+}
 
 function ChooseDeck(deckPicked) {
-    let deckShuffled = [...deckPicked];
-    deckShuffled.sort(() => Math.random() - 0.5);
-    deckShuffled = deckShuffled.filter((e, i, a) => i < a.length ? e : 0);
-    return deckShuffled;
-};
+  let deckShuffled = [...deckPicked];
+  deckShuffled.sort(() => Math.random() - 0.5);
+  deckShuffled = deckShuffled.filter((e, i, a) => (i < a.length ? e : 0));
+  return deckShuffled;
+}
 
 function RenderCards(deckQuestion, index, result) {
-    const [state, setState] = react.useState([0, 0]);
-    const [answered, setAnswered] = react.useState('A');
-    const [size, setSize] = react.useState(['notAnswered', 'notAnswered']);
-    if (!result.includes(size) && size[1] !== 'notAnswered') {
-        result.push(size);
-    };
-    if (state[0] === 0) {
-        return Question(index, Select, state, setState);
-    };
-    if (state[0] === 1 && state[1] === 0) {
-        return QuestionSelected(deckQuestion, Flip, setState);
-    };
-    if (state[0] === 2 && state[1] === 0) {
-        return QuestionAnswered(deckQuestion, Select, setState, answered, setAnswered, index, setSize);
-    };
-};
-
-
+  const [state, setState] = react.useState([0, 0]);
+  const [answered, setAnswered] = react.useState('A');
+  const [size, setSize] = react.useState(['notAnswered', 'notAnswered']);
+  if (!result.includes(size) && size[1] !== 'notAnswered') {
+    result.push(size);
+  }
+  if (state[0] === 0) {
+    return Question(index, Select, state, setState);
+  }
+  if (state[0] === 1 && state[1] === 0) {
+    return QuestionSelected(deckQuestion, Flip, setState);
+  }
+  if (state[0] === 2 && state[1] === 0) {
+    return QuestionAnswered(
+      deckQuestion,
+      Select,
+      setState,
+      answered,
+      setAnswered,
+      index,
+      setSize
+    );
+  }
+}
